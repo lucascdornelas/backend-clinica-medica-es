9226010c2e8ebcc51f524183c72a2308d9565e2f
lucascdornelas <lucascdornelas@gmail.com>
2022-11-02 17:24:45 -0300

    feat: confi lint and formatter

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -20,12 +20,10 @@ module.exports = {
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
-    "quotes": ["error", "double"],
+    quotes: ["error", "double"],
     "import/no-unresolved": 0,
+    "object-curly-spacing": ["error", "always"],
   },
 };
